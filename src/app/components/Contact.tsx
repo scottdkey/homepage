a12f24e3f5f32db9ca9943727b1c96eac23f7dc0
Scott Key <scottdkey@gmail.com>
2024-03-11 11:58:13 -0600

    added contact links

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,7 +1,38 @@
+import Image from "next/image";
+import LinkedInLogo from "../images/LinkedIn_icon.svg";
+import "./Contact.css";
+
 export const Contact = () => {
   return (
-    <>
-      <button>Linkedin</button>
-    </>
+    <div className="container">
+      <a
+        href={"https://linkedin.com/in/scottdkey"}
+        className="linkedin-button"
+        target="_blank"
+      >
+        <Image
+          className="logo-image"
+          src={LinkedInLogo}
+          alt="linkedin logo"
+          width={40}
+          height={40}
+        />
+        <div className="btn-text">Profile</div>
+      </a>
+      <a
+        href={"https://github.com/scottdkey"}
+        className="linkedin-button"
+        target="_blank"
+      >
+        <Image
+          className="logo-image"
+          src={LinkedInLogo}
+          alt="linkedin logo"
+          width={40}
+          height={40}
+        />
+        <div className="btn-text">Github</div>
+      </a>
+    </div>
   );
 };
